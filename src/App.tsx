7a028ec3aae43638ae4b1adda33471292ef3e15e
Paulo G Reis <paulosantos--@live.com>
2023-04-19 11:01:03 -0400

    add dependência react-router-dom e cria e confiura as paginas e rotas base da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
-
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
