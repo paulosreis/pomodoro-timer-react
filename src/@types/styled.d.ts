f8fb70f1e70c8af7f9bba9bddc7b9383db9dff36
Paulo G Reis <paulosantos--@live.com>
2023-04-19 10:31:38 -0400

    configura ESLINT e aplica correções

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,10 +1,10 @@
-//Arquivo de definição de tipos
+// Arquivo de definição de tipos
 
-import 'styled-components';
-import { defaultTheme } from '../styles/themes/default';
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
 
-type ThemeType = typeof defaultTheme;
+type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-    export interface DefaultTheme extends ThemeType { }
+  export interface DefaultTheme extends ThemeType {}
 }
